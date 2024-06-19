ed9d6803d7472aa8bff4e5d18aa96626b9c6608c
Adarshm22 <149037565+Adarshm22@users.noreply.github.com>
2024-06-19 19:13:52 +0530

    Add files via upload

---
+++ assets/php/actions.php
@@ -0,0 +1,118 @@
+<?php
+require_once 'functions.php';
+
+if(isset($_GET['test'])){
+ 
+}
+
+if(isset($_GET['deletepost'])){
+    $post_id = $_GET['deletepost'];
+      if(deletePost($post_id)){
+          header("location:{$_SERVER['HTTP_REFERER']}");
+      }else{
+          echo "something went wrong";
+      }
+  
+    
+}
+
+
+
+//for signup
+if(isset($_GET['signup'])){
+$response=validateSignupForm($_POST);
+if($response['status']){
+    if(createUser($_POST)){
+    header('location:../../?login&newuser');
+    }else{
+        echo "<script>alert('somethihng is wrong')</script>";
+    }
+   
+
+}else{
+    $_SESSION['error']=$response;
+    $_SESSION['formdata']=$_POST;
+    header("location:../../?signup");
+}
+    
+}
+
+
+
+//for login
+if(isset($_GET['login'])){
+
+  
+    $response=validateLoginForm($_POST);
+  
+    if($response['status']){
+     $_SESSION['Auth'] = true;
+     $_SESSION['userdata'] = $response['user'];
+    header("location:../../");
+
+    }else{
+        $_SESSION['error']=$response;
+        $_SESSION['formdata']=$_POST;
+        header("location:../../?login");
+    }
+}
+
+//for logout
+if(isset($_GET['logout'])){
+    session_destroy();
+    header('location:../../');
+
+}
+
+if(isset($_GET['changepassword'])){
+    if(!$_POST['password']){
+        $response['msg']="enter your new password";
+        $response['field']='password';
+        $_SESSION['error']=$response;
+        header('location:../../?forgotpassword');
+    }else{
+        resetPassword($_SESSION['forgot_email'],$_POST['password']);
+        session_destroy();
+        header('location:../../?reseted');
+    }
+
+
+}
+
+
+if(isset($_GET['updateprofile'])){
+
+    $response=validateUpdateForm($_POST,$_FILES['profile_pic']);
+
+    if($response['status']){
+       
+        if(updateProfile($_POST,$_FILES['profile_pic'])){
+            header("location:../../?editprofile&success");
+
+        }else{
+            echo "something is wrong";
+        }
+       
+    
+    }else{
+        $_SESSION['error']=$response;
+        header("location:../../?editprofile");
+    }
+     
+}
+
+//for add post
+if(isset($_GET['addpost'])){
+   $response = validatePostImage($_FILES['post_img']);
+
+   if($response['status']){
+if(createPost($_POST,$_FILES['post_img'])){
+    header("location:../../?new_post_added");
+}else{
+    echo "something went wrong";
+}
+   }else{
+    $_SESSION['error']=$response;
+    header("location:../../");
+   }
+}
